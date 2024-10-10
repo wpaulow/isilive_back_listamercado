9614fa11160ff4da19b9b8fa24f02694b65ca93f
wpaulow <paulow.wesley33@gmail.com>
2024-10-09 21:15:41 -0300

    maldita importacao do DeleteMapping

--- src/main/java/br/com/wesisiflix/appmercado/controller/ItemListaController.java
+++ src/main/java/br/com/wesisiflix/appmercado/controller/ItemListaController.java
@@ -2,6 +2,7 @@ package br.com.wesisiflix.appmercado.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
